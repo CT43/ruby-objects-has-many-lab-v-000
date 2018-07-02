bf063749cb8950e101cf7aa3273fd9f922a5a773
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-02 22:31:50 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -2,7 +2,19 @@
 class Author
   attr_accessor :name
 
+
+
   def initialize(name)
     @name = name
+    @posts = []
   end
+
+  def posts
+    @posts
+  end
+
+  def add_post(post)
+    post.author = self
+
+
 end
